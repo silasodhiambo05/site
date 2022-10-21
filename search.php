783a0b78c07b4173269c4d6a5c34c49d7bafd91b
Silas Odhiambo <silasodhiambokibu@gmail.com>
2022-10-21 13:10:40 +0300

    Commit

---
+++ search.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <meta name = "format-detection" content = "telephone=no" />
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">   
+    <meta name = "format-detection" content = "telephone=no" /> 
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/responsive.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="search/search.js"></script>
+    <script type="text/javascript" src="js/superfish.js"></script>
+    <script type="text/javascript" src="js/jquery.mobilemenu.js"></script>
+    <!--[if lt IE 8]>
+        <div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/img/upgrade.jpg"border="0"alt=""/></a></div>  
+    <![endif]-->
+    <!--[if lt IE 9]>
+      <link rel="stylesheet" href="css/ie.css" type="text/css" media="screen">
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+<!--==============================header=================================-->
+<header>
+    <div class="container">
+         <div class="navbar navbar_ clearfix">
+            <div class="navbar-inner">      
+                  <div class="clearfix">
+                    <h1 class="brand"><a href="index.html"><img src="img/logo.png" alt=""></a><span>hotel luxury</span></h1> 
+                    <div class="nav-collapse nav-collapse_ collapse">
+                    <a href="#"></a>
+                        <ul class="nav sf-menu clearfix">
+                          <li><a href="index.html">Home <span>...welcome page</span></a></li>
+                          <li class="sub-menu"><a href="index-1.html">Rooms <span>...packages &amp; deals</span></a>
+                             <ul>
+                              <li><a href="#">Corporate</a></li>
+                              <li class="sub-menu-1"><a href="#">Family</a>
+                                <ul>
+                                  <li><a href="#">Gallery</a></li>
+                                  <li><a href="#">Activities</a></li>
+                                  <li><a href="#">Offers</a></li>
+                                  <li><a href="#">Bonus</a></li>
+                                </ul>
+                              </li>
+                              <li><a href="#">Honeymoon</a></li>
+                              <li><a href="#">Faqs</a></li>
+                             </ul>
+                          </li>
+                          <li><a href="index-2.html">Booking <span>...prices &amp;  offers</span></a></li>
+                          <li><a href="index-3.html">About <span>...who we are</span></a></li>
+                          <li><a href="index-4.html">Mail <span>...stay in touch</span></a></li>
+                        </ul>
+                    </div>
+                  </div>
+             </div>  
+         </div>
+    </div>
+</header>
+<section id="content">
+  <div class="container">
+    <div class="row">
+      <div class="span12">
+       <h2 class="h2-pad">Search result:</h2>
+        <div id="search-results"></div>
+      </div>
+    </div>
+  </div>
+</section>
+<footer>
+  <div class="container">
+       <div class="row">
+            <article class="span6 fright">
+             <ul class="menu-footer">
+               <li><a href="index.html">Home</a></li>
+               <li><a href="index-1.html">rooms</a></li>
+               <li><a href="index-2.html">booking</a></li>
+               <li><a href="index-3.html">about</a></li>
+               <li><a href="index-4.html">mail</a></li>
+             </ul>
+           </article>
+           <article class="span6 fleft">
+                 <span>Hotel luxury &copy; 2013 | &nbsp;&nbsp;</span><a href="index-5.html">Privacy Policy</a>
+           </article>
+           
+       </div>
+  </div>
+</footer>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+</body>
+</html>
